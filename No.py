8298917fc18672fd1e118955c987de035f129a57
Thorfinn-Karlsefniest <m.prottoy.siddique@gmail.com>
2024-10-23 18:38:11 -0400

    Huh>???

---
+++ No.py
@@ -0,0 +1,9 @@
+def fleshgod_apocolypse():
+    sound = 0
+    while sound < 10:
+        print("Not yet")
+        sound += 1
+    while sound == 10:
+        print("Done")
+
+print(fleshgod_apocolypse())
